47fdcf47d931476ed923ed9ac414ad136d1f0858
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-03-01 21:00:36 +0000

    Update Main.java

--- Ficha2/Main.java
+++ Ficha2/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TestePrograma {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite o número do exercício: ");
